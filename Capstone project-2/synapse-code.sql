13294a15940fcf6fc3f770b18694a4fb760b9532
Afnan Abdullah <afnan98abdullah@gmail.com>
2024-06-20 10:29:52 +0300

    DE

---
+++ Capstone project-2/synapse-code.sql
@@ -0,0 +1,10 @@
+-- This is auto-generated code
+SELECT
+    TOP 5 *
+FROM
+    OPENROWSET(
+        BULK 'https://capstoneproject2as.dfs.core.windows.net/bd-project/BI/ml_result.csv',
+        FORMAT = 'CSV',
+        PARSER_VERSION = '2.0',
+        HEADER_ROW=TRUE
+    ) AS [result]
